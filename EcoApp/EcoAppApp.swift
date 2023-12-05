6b70b4a1b9fd7bf1a553ebe1b14bd34c5a2c0cd3
AllanJones24 <allanjonesjr24@gmail.com>
2023-12-05 14:49:07 -0600

    Initial Commit

---
+++ EcoApp/EcoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  EcoAppApp.swift
+//  EcoApp
+//
+//  Created by Allan Jones on 12/5/23.
+//
+
+import SwiftUI
+
+@main
+struct EcoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
